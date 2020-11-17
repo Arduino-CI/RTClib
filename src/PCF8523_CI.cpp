05707f5d122c8819490573cc821033035c6497a1
James Foster <github@jgfoster.net>
2020-11-16 18:16:51 -0800

    Refactor includes to get tests to pass.

--- src/PCF8523_CI.cpp
+++ src/PCF8523_CI.cpp
@@ -1,7 +1,6 @@
-#if defined(MOCK_PINS_COUNT)
-
-#include <RTClib.h>
 #include <PCF8523_CI.h>
+
+#if defined(MOCK_PINS_COUNT)
 
 bool RTC_PCF8523_CI::isInitialized = false;
 uint32_t RTC_PCF8523_CI::offset = 0;
--- src/PCF8523_CI.h
+++ src/PCF8523_CI.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Arduino.h>
+
 #if defined(MOCK_PINS_COUNT)
 
 #include <RTClib.h>
